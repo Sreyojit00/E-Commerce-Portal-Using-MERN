dd76c00026c11d189a3a7c2ddceb1a6044d1bf48
Sreyojit Khasnobish <sreyajitlm10.5@gmail.com>
2024-10-18 10:00:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const cookieParser = require('cookie-parser')
 const app=express()
 app.use(cors({
 
-    origin : process.env.FRONTEND_URL,
+    origin : "https://sreyojit-ecommerce-2k24.netlify.app" //process.env.FRONTEND_URL,
     credentials : true
 
 }))
